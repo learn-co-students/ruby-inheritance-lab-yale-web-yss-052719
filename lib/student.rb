59a4dec5bedd75702ff61209deaece07a13da52c
github username <github email address>
2019-05-29 21:05:13 -0400

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
 
-end
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(str)
+        @knowledge << str
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+
+end 
